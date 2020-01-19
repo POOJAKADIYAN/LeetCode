7ac1d762864549510a0fd81e4f204ce74e74f087
Pooja Kadiyan <poojakad123@gmail.com>
2020-01-19 20:16:00 +0530

    new year chaos

--- New year chaos.cpp
+++ New year chaos.cpp
@@ -4,7 +4,7 @@
 // Any person in the queue can bribe the person directly in front of them to swap positions. If two people
 // swap positions, they still wear the same sticker denoting their original places in line. One person can bribe
 // at most two others. For example, if n=8 and person 5 bribesperson 4 , the queue will look like this:
-// 1,2,3,5,4,6,7,8.
+// 1,2,3,5,4,6,7,8
 // Fascinated by this chaotic queue, you decide you must know the minimum number of bribes that took
 // place to get the queue into its current state!
 // Function Description
