b463935fcde741255558d6fbf03601da3e7b9298
Pooja Kadiyan <poojakad123@gmail.com>
2020-11-29 03:37:00 +0530

    More BT algo

---
+++ Balanced BT.cpp
@@ -0,0 +1,54 @@
+// Given a binary tree, determine if it is height-balanced.
+
+// > Height-balanced binary tree : is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+// > 
+
+// Return `0 / 1` ( 0 for false, 1 for true ) for this problem
+
+// **Example :**
+
+// `Input : 1`
+
+//            `/ \`
+
+//            `2 3
+// Return : True or 1`
+
+// `Input 2 : 3` 
+
+//              `/`
+
+//           `2` 
+
+//        `/`
+
+//      `1
+// Return : False or 0 Because for the root node, left subtree has depth 2 and right subtree has depth 0. Difference = 2 > 1.`
+
+// ## **SOLUTION -**
+
+int depth (TreeNode *A)
+{
+    if (A==NULL) 
+        return 0;
+    int x,y;
+    x=depth(A->left);
+    y=depth(A->right);
+    if (x==-1 || y==-1) 
+        return -1;
+    else if (abs(x-y)>1)
+        return -1;
+    else 
+        return max(x,y)+1;
+} 
+int Solution::isBalanced(TreeNode* A) 
+{
+    if (A==NULL) 
+        return 1;
+    else if (depth(A->left)>=0 && depth(A->right)>=0)
+    {
+        if (abs(depth(A->left)-depth(A->right))<2)
+            return 1;
+    } 
+    return 0;
+}
